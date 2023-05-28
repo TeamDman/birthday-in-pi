be561ba381cb305ec328ac4d85bd8ddb3d8eacd2
TeamDman <TeamDman9201@gmail.com>
2023-05-27 20:47:03 -0400

    back to start

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,33 +1,19 @@
-use memmap::MmapOptions;
 use std::fs::File;
-use std::time::Instant;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-fn main() -> std::io::Result<()> {
-    let start = Instant::now();
+fn main() -> io::Result<()> {
+    let path = Path::new("../../resources/pi_dec_1m.txt");
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
 
-    let file = File::open("../resources/pi-billion.txt")?;
-    let map = unsafe { MmapOptions::new().map(&file)? };
-
-    let mut sequence_counts = vec![0; 2_000_000];
-    let mut sequence = Vec::with_capacity(8);
-
-    for &byte in map.iter().skip(2) {
-        sequence.push(byte - b'0');
-        if sequence.len() == 8 {
-            let date = sequence.iter().fold(0, |acc, &digit| acc * 10 + (digit as usize));
-            sequence.remove(0);
-            if date >= 1_900_0000 && date < 2_100_0000 {
-                sequence_counts[date - 1_900_0000] += 1;
+    for line in reader.lines() {
+        let line = line?;
+        for char in line.chars() {
+            if char.is_digit(10) {
+                println!("{}", char);
             }
         }
     }
-
-    let total_unique_dates = sequence_counts.into_iter().filter(|&count| count > 0).count();
-
-    let duration = start.elapsed();
-    println!("Counted {} unique dates, took {} ms", total_unique_dates, duration.as_millis());
-
     Ok(())
 }
-
-// took 484224
